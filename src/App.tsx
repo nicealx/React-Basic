28733e139dae1b6feb3e05cbb77f16ecf9cc68b7
nicealx <kav.prok@gmail.com>
2024-04-28 21:42:30 +0700

    init: add base files and settings for React

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import React = require('react');
+
+const App = () => <h1>React TypeScript Webpack Starter Template</h1>;
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const devTool = devMode ? 'inline-source-map' : undefined;
+const devServer = devMode ? { static: path.resolve(__dirname, './dist') } : undefined;
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index'),
+  mode: mode,
+  devtool: devTool,
+  devServer: devServer,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.tsx$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, './build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      // favicon: path.resolve(__dirname, './src/favicon.ico'),
+    }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: 'ts' }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+};
