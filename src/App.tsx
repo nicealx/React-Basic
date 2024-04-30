76081457d551584afc25a1dc41953e74672d1d5b
nicealx <kav.prok@gmail.com>
2024-04-30 15:04:38 +0700

    refactor: test scripts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App = () => (
   <div className={styles.App}>
     <h1>React TypeScript Webpack Starter Template</h1>
     <img className={styles.App__image} src="./images/react.svg" alt="react-logo" />
+    <p className={styles.App__text}>Text</p>
   </div>
 );
 export default App;
